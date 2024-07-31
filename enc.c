a91a53745a0cb36ee3d2898dda6fc7c86c6da2e1
yel1337 <daniel.salvaloza2002@gmail.com>
2024-08-01 03:21:17 +0800

    Changes

--- enc.c
+++ enc.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #define SQLITE_HAS_CODEC 1
-#define TMPMAX 32
 #define AUTH_ERROR 0
 #define AUTH_SUCCESS 1
 
@@ -81,185 +80,102 @@ int decrypt(unsigned char *ciphertext, int ciphered_len, unsigned char *key, uns
 
 	return plaintext_len; 
 }
-static int callback( void *data, int argc, char **argv, char **azColName)
+static int credentials_callback(void *data, int argc, char **argv, char **azColName)
 {
     int i;
 
     for(i = 0; i < argc; i++)
     {
-        printf("key: %s\n", argv[i]);
+        printf("%s\n", azColName[i]);
     }
 }
-// Function prototpes
-static int key_auth_check(unsigned char *key, FILE *fp_auth);
-const char *db_dump(const char *PRAGMA_TMP, FILE *fp, size_t write_pragma, int rc);
+const int key_auth_check(unsigned char *key, FILE *fp_auth);
+void insert_data_to_db(sqlite3 *db, int rc, unsigned char *kdf_key, char *username)
+{
+        sqlite3_stmt *stmt;
+
+        // Prepare
+        rc = sqlite3_prepare_v2(db, "INSERT INTO credentials(id, username, password), VALUES(?, '?', '?');", -1, &stmt, 0);
+
+        // Bind text
+        rc = sqlite3_bind_text(stmt, 2, username, -1, SQLITE_STATIC);
+
+        // Step
+        rc = sqlite3_step(stmt);
+
+        // Finalize
+        rc = sqlite3_finalize(stmt);
+}
 
 int main()
 {
-    char *zErrMsg = 0;
-
-    // Key derivation using PBKDF2 from openssl
-    unsigned char *key;
-
-    scanf("openssl kdf -keylen 32 -kdfopt digest:SHA256 -kdfopt pass:%s -kdfopt salt:salt -kdfopt iter:2 PBKDF2", &key);
-
-    // Generate IV from CSPRNG
-    unsigned char *buf = malloc(16 *sizeof(*buf)); // Rand buffer
-    int rand_gen = RAND_bytes(buf, 16);
-    unsigned char *rand_iv = (unsigned char *)malloc(16 * sizeof(rand_gen));
-    unsigned char *iv = rand_iv;
-
-    // Initialize cipher context
-    EVP_CIPHER_CTX *ctx;
-    ctx = EVP_CIPHER_CTX_new();
-    EVP_CIPHER_CTX_init(ctx);
-
-    // Buffer for ciphered text
-    unsigned char ciphertext[128];
-
-    // Buffer for decryptedtext
-    unsigned char decryptedtext[128];
-
-    int decryptedtext_len, ciphered_len; 
-
-    // Encrypt
-    ciphered_len = encrypt(plaintext, strlen((char *)plaintext), key, iv, ciphertext);
-	
-    // Decrypt
-    decryptedtext_len = decrypt(ciphertext, ciphered_len, key, iv, decryptedtext);
-
-    // Add a NULL terminator 
-    decryptedtext[decryptedtext_len] = '\0';
 
     /*
-     * SQLite3
-     *
      * For every insert operation (or update, delete and select) call sqlite3_step(stmt)
      */
     sqlite3 *db;
     int rc;
-
-    rc = sqlite3_open("pass_man_db.db", &db); // Open db statement
-
-    if(rc == SQLITE_ERROR )
-    {
-        fprintf(stderr, "%s\n", sqlite3_errmsg(db));
-        return(0);
-    } else if(rc == SQLITE_OK) {
-        rc = sqlite3_exec(db, "ATTACH DATABASE 'pass_man_db_encrypted.db' AS pass_man_db_encrypted KEY 'key'", 0, 0, &zErrMsg); // Pragma key not yet done
-
-        rc = sqlite3_exec(db, "SELECT sqlcipher_export('pass_man_db_encrypted');");
-
-        rc = sqlite3_exec(db, "DETACH DATABASE pass_man_db_encrypted;");
-    }
-
-    // Prepare
-    char *sql;
-    sqlite3_stmt *stmt;
-
-    // Prepare
-    rc = sqlite3_prepare_v2(db, "INSERT INTO key VALUES(?);", -1, &stmt, 0);
-    if(rc != SQLITE_OK)
-    {
-       fprintf(stderr, "SQL error: %s\n", zErrMsg);
-       sqlite3_free(zErrMsg);
-    }
-
-    // Bind text
-    rc = sqlite3_bind_text(stmt, 1, key, -1, SQLITE_STATIC);
-    if(rc != SQLITE_OK)
-    {
-       fprintf(stderr, "SQL error: %s\n", zErrMsg);
-       sqlite3_free(zErrMsg);
-    }
-
-    // Step
-    rc = sqlite3_step(stmt);
-    if(rc != SQLITE_DONE)
-    {
-       fprintf(stderr, "SQL error: %s\n", zErrMsg);
-       sqlite3_free(zErrMsg);
-    }
-
-    // Free
-    rc = sqlite3_finalize(stmt);
-
+    char *zErrMsg = 0;
     FILE *fp;
-    static int key_auth_check;
-    size_t write_pragma;
-    const char *PRAGMA_TMP;
-    const char *db_dump;
+    int key_auth;
 
-    key_auth_check = key_auth_check(key, fp, db, rc); // Do key_auth_check
+    key_auth = key_auth_check(kdf_key, fp); // Do key_auth_check
 
     /*
      * key_auth_check = 0 // AUTH_ERROR
      * key_auth_check = 1 // AUTH_SUCCESS
      */
-    if(key_auth_check == 1)
+    if(key_auth == 1)
     {
-        db_dump = db_dump(PRAGMA_TMP, fp, write_pragma, rc, db);
+        fp = popen("sqlcipher pass_man.db", "w");
 
-        return 0;
+        rc = sqlite3_exec(db, "SELECT * FROM credentials", 0, 0, &zErrMsg);
+
+        if(rc == SQLITE_ERROR)
+        {
+            fprintf(stderr, "SQL ERROR: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }
+
+        pclose(fp);
     } else if(key_auth_check == 0) {
         fprintf(stderr, "PRAGMA Key AUTH_FAILED");
         exit(EXIT_FAILURE);
     }
 
+    char *username = "Daniel";
+
+    insert_data_to_db(db, rc, kdf_key, username);
+
     sqlite3_close(db);
 }
 
 /*
- * AUTH
+ * KEY_AUTH
  */
-static int key_auth_check(unsigned char *key, FILE *fp_auth, sqlite3 *db, int rc )
+int key_auth_check(kdf_key, FILE *fp)
 {
-        char *zErrMsg = 0;
+        fp = popen("sqlcipher test.db", "w");
 
-        fp = popen("sqlcipher pass_man_db_encrypted.db", "w");
+        char *sprintf_buffer = malloc(128 *sizeof(char));
 
-        if(fp == NULL)
+        /*
+         * kdf_key defined from makefile preprocessor
+         */
+        int pragma_key_auth = sprintf(sprintf_buffer, "PRAGMA key = '%s'", kdf_key);
+
+        if(pragma_key_auth != sizeof(sprintf_buffer))
         {
-            perror("popen sqlcipher failed\n"); //
-
-            exit(EXIT_FAILURE);
-        }
-
-        if(fp != NULL)
-        {
-            unsigned char *auth_pragma = "PRAGMA key='%s';", enc_key; // enc_key as PRAGMA key
-            size_t write_auth_pragma = fwrite(auth_pragma, sizeof(char), strlen(auth_pragma), fp);
-
-            if(write_auth_pragma != strlen(auth_pragma))
-            {
-                fprintf(stderr, "auth_pragma failed\n");
-            }
-
-            rc = sqlite3_exec(db, "SELECT * FROM key;", callback, 0, &zErrMsg);
-
-            if(rc == SQLITE_ERROR) // If rc == SQLITE_ERROR -> Query will exec || enc_key is not the correct PRAGMA key
-            {
-                fprintf(stderr, "SQL error: AUTH_FAILED\n");
-
-                exit(EXIT_FAILURE);
+                fprintf(stderr, "pragma_key_auth: ERROR\n");
 
                 return AUTH_ERROR;
-            }
-            if(rc == SQLITE_OK)
-            {
+
+                exit(EXIT_FAILURE);
+        } else if(pragma_key_auth == sizeof(sprintf_buffer)) {
                 return AUTH_SUCCESS;
-            }
-
-            pclose(fp); // Close pipe
         }
-}
-const char *db_dump(const char *PRAGMA_TMP, FILE *fp, size_t write_pragma, int rc, sqlite3 *db;)
-{
-        rc = sqlite3_exec(db, "SELECT * FROM key;", callback, 0, &zErrMsg);
 
-        if(rc == SQLITE_ERROR)
-        {
-             fprintf(stderr, "SQL error: ERROR DUMPING DB\n");
-        }
+        pclose(fp); // Close pipe
 }
+
 
--- makefile
+++ makefile
@@ -1,9 +1,17 @@
 # Variables
-kdf_key = $(shell sh -c 'echo -n "Enter password: " && read -s key && echo && openssl kdf -keylen 32 -kdfopt digest:SHA256 -kdfopt pass:$$key -kdfopt salt:salt -kdfopt iter:2 PBKDF2')
-CFLAGS = -D kdf_key = \"$(kdf_key)\"
+kdf_key = $(shell sh -c 'openssl kdf -keylen 32 -kdfopt digest:SHA256 -kdfopt pass:pass -kdfopt salt:salt -kdfopt iter:2 PBKDF2') 
+CC = gcc
+CFLAGS = -I -L -l -o -D kdf_key = \"$(kdf_key)\"
+LFAGS = -lssl -lsqcipher -lssl -lcrypto -l
 db = pass_man.db
-sqlcipher_cmd = ./sqlcipher
+src = enc.c
+EXEC = enc
+sqlcipher_cmd = sqlcipher
 db_sql = db_setup.sql 
+
+all:
+	$(kdf_key)
+	$(CC) -o $(EXEC) $(src) -I /usr/local/include/sqlcipher/ -L /usr/local/lib/libsqlcipher.a -lssl -lsqlcipher -lssl -lcrypto -l sqlite3 
 
 # DB Setup
 db_setup:
