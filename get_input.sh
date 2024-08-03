4360b3a99598f89cf4ceefa11426b9b907360b2c
yel1337 <yel1337@github.com>
2024-08-03 19:21:31 +0800

    Minor changes

--- get_input.sh
+++ get_input.sh
@@ -1,1 +1,7 @@
-read -p "Enter password: " input 
+read -s -p "Enter password: " input 
+echo 
+
+key=$(openssl kdf -keylen 32 -kdfopt digest:SHA256 -kdfopt pass:$input -kdfopt salt:salt -kdfopt iter:2 PBKDF2)
+echo "$key" | openssl enc -aes-256-cbc -salt -out keyfile.enc -pass pass:password
+
+
